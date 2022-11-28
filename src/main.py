0c0ed4b0f7509196380e24c03925fb31e5dc040a
user36672 <anton.ingrish@gmail.com>
2022-11-28 16:10:28 +0300

    refactor: move world filling back to Game class
    Move the initialization of the world to the method of the Game class
    from the WorldInitializer class

--- src/main.py
+++ src/main.py
@@ -6,14 +6,30 @@ FPS = 60
 RESOLUTION = 720, 480
 
 
-class WorldInitializer:
-    def __init__(self):
-        world = esper.World()
+class Game:
+    def __init__(self, window, world=None):
+        self.window = window
+        self.world = world
 
-        world.add_entity(self._create_object_entities())
-        world.add_entity(self._create_ui_entities())
+    def tick(self):
+        self.clock.tick_busy_loop(FPS)
 
-        return world
+    def run(self):
+        self.clock = pygame.time.Clock()
+        pygame.key.set_repeat(1, 1)
+
+        if self.world is None:
+            self._init_world()
+
+        running = True
+        while running:
+            self.tick()
+
+    def _init_world(self):
+        self.world = esper.World()
+
+        self.world.add_entity(self._create_object_entities())
+        self.world.add_entity(self._create_ui_entities())
 
     def _create_decoration_objects(self):
         pass
@@ -58,26 +74,6 @@ class WorldInitializer:
                 self._create_dialog_ui()
             )
         )
-
-
-class Game:
-    def __init__(self, window, world=None):
-        self.window = window
-        self.world = world
-
-    def tick(self):
-        self.clock.tick_busy_loop(FPS)
-
-    def run(self):
-        self.clock = pygame.time.Clock()
-        pygame.key.set_repeat(1, 1)
-
-        if self.world is None:
-            self.world = WorldInitializer()
-
-        running = True
-        while running:
-            self.tick()
 
 
 if __name__ == "__main__":
