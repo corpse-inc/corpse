13b73ba6769f8303025962b61c03a7675eb3cf58
Mark Meliksetyan <markmelix@gmail.com>
2022-11-29 14:01:41 +0300

    ECSize the game

---
+++ src/location.py
@@ -0,0 +1,12 @@
+import pygame
+import esper
+
+
+class LocationProcessor(esper.Processor):
+    def process(self, dt, running):
+        pass
+
+
+class PositionProcessor(esper.Processor):
+    def process(self, dt, running):
+        pass
---
+++ src/movement.py
@@ -0,0 +1,7 @@
+import pygame
+import esper
+
+
+class MovementProcessor(esper.Processor):
+    def process(self, dt, running):
+        pass
