de4a38e0d7f8e50db01e466b67be475d70100a2b
Mark Meliksetyan <markmelix@gmail.com>
2022-12-02 19:27:47 +0300

    fix: yet another fix for making paths crossplatform

--- src/utils.py
+++ src/utils.py
@@ -18,7 +18,7 @@ PLAYER_SPEED = 3
 
 def init_resources_path() -> str:
     res = "/".join(__file__.split("/")[:-2]) + "/resources"
-    if res.startswith("/"):
+    if sys.platform not in {"linux", "darwin"} and res.startswith("/"):
         return res[1:]
     return res
 
@@ -26,25 +26,14 @@ def init_resources_path() -> str:
 RESOURCES = init_resources_path()
 
 
-def crossplatform_path(path: str) -> str:
-    return path
-    # if sys.platform == "posix" or not sys.platform.startswith("win"):
-    # return path
-    # return path.replace("/", "\\")
-
-
 class ResourcePath:
     @classmethod
     def location_tilemap(cls, location_id: str) -> str:
-        return crossplatform_path(
-            f"{RESOURCES}/locations/tilemaps/{location_id}/tilemap.tmx"
-        )
+        return f"{RESOURCES}/locations/tilemaps/{location_id}/tilemap.tmx"
 
     @classmethod
     def creature_frame(cls, creature: str, state: AnimationState, idx: int) -> str:
-        return crossplatform_path(
-            f"{RESOURCES}/creatures/{creature}/{state.name}/{idx}.png"
-        )
+        return f"{RESOURCES}/creatures/{creature}/{state.name}/{idx}.png"
 
 
 def surface_from_animation(animation: Animation) -> pygame.surface.Surface:
