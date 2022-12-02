2fa3128bc118f5f60eb9e8e4a379e4d44dbdf878
Mark Meliksetyan <markmelix@gmail.com>
2022-12-02 23:23:58 +0300

    feat: basic roofs implementation

--- src/roof.py
+++ src/roof.py
@@ -1,6 +1,35 @@
+import pygame
 import esper
+
+from creature import PlayerMarker
+from location import Layer, Location, Position
 
 
 class RoofTogglingProcessor(esper.Processor):
     def process(self, **_):
-        pass
+        for _, (_, pos) in self.world.get_components(PlayerMarker, Position):
+            location = self.world.component_for_entity(pos.location, Location)
+            coords = pos.coords
+
+            tile_width = location.map.tilewidth
+            tiles = map(
+                lambda t: (
+                    t,
+                    pygame.Vector2(t[0] * tile_width, t[1] * tile_width),
+                    pygame.Vector2(
+                        t[0] * tile_width + tile_width - 1,
+                        t[1] * tile_width + tile_width - 1,
+                    ),
+                ),
+                location.map.layers[Layer.Roofs].tiles(),
+            )
+
+            for tile, start_coords, end_coords in tiles:
+                surface = tile[-1]
+                if (
+                    start_coords.x <= coords.x <= end_coords.x
+                    and start_coords.y <= coords.y <= end_coords.y
+                ):
+                    surface.set_alpha(0)
+                else:
+                    surface.set_alpha(1000)
