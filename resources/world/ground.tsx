b060e06c687411cfd4efc54695758c6237ffc826
Mark Meliksetyan <markmelix@gmail.com>
2022-12-08 16:59:21 +0300

    feat: new assets and basic map creation

---
+++ resources/world/ground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="ground" tilewidth="64" tileheight="64" tilecount="5" columns="1">
+ <image source="../assets/tds_modern_pixel_game_kit/tilesets/PNG/Tiles/Tiles.png" width="64" height="320"/>
+</tileset>
