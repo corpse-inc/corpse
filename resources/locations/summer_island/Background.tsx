e9c6b5546e01eade746183a835ca790b82ab2d32
Anthropophag <anton.ingrish@gmail.com>
2022-12-09 05:45:20 +0300

    Add ground layer

---
+++ resources/locations/summer_island/Background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Background" tilewidth="32" tileheight="32" tilecount="1440" columns="45">
+ <image source="../../assets/background/Source/Tileset.png" width="1440" height="1024"/>
+</tileset>
