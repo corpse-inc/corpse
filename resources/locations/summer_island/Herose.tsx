1198c434f7dcd983d34752d82325f362102de38a
Anthropophag <anton.ingrish@gmail.com>
2022-12-11 16:47:46 +0300

    Create vertol locate

--- resources/locations/summer_island/Herose.tsx
+++ resources/locations/summer_island/Herose.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Herose" tilewidth="64" tileheight="65" tilecount="6" columns="0">
+<tileset version="1.9" tiledversion="1.9.2" name="Herose" tilewidth="64" tileheight="74" tilecount="10" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="64" height="64" source="../../assets/herose/Hero_Pistol/Hero_Pistol.png"/>
@@ -18,5 +18,17 @@
  </tile>
  <tile id="5">
   <image width="47" height="58" source="../../../../../Downloads/zombi2 (5).png"/>
+ </tile>
+ <tile id="7">
+  <image width="47" height="74" source="../../../../../Downloads/zombi2 (6).png"/>
+ </tile>
+ <tile id="8">
+  <image width="39" height="41" source="../../../../../Downloads/zombi2 (7).png"/>
+ </tile>
+ <tile id="9">
+  <image width="53" height="59" source="../../../../../Downloads/zombi2 (8).png"/>
+ </tile>
+ <tile id="10">
+  <image width="37" height="40" source="../../../../../Downloads/zombi2 (9).png"/>
  </tile>
 </tileset>
