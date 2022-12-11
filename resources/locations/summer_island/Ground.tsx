b5cfb31c7eb71aedcd9e5ef53a413c26bb2b674a
Anthropophag <anton.ingrish@gmail.com>
2022-12-11 04:17:01 +0300

    Add asset for house roofe

---
+++ resources/locations/summer_island/Ground.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Ground" tilewidth="320" tileheight="320" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image width="320" height="320" source="../../assets/background/Tiles/_0000_WTiles.png"/>
+ </tile>
+ <tile id="2">
+  <image width="320" height="320" source="../../assets/background/Tiles/_0001_DirtTiles.png"/>
+ </tile>
+</tileset>
