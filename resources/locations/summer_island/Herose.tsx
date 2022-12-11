3f8e4de47b2d442f1d9375dc6c4f0717e3aaabfd
Anthropophag <anton.ingrish@gmail.com>
2022-12-11 14:36:41 +0300

    Add trees and house

--- resources/locations/summer_island/Herose.tsx
+++ resources/locations/summer_island/Herose.tsx
@@ -1,7 +1,22 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Herose" tilewidth="64" tileheight="64" tilecount="1" columns="0">
+<tileset version="1.9" tiledversion="1.9.2" name="Herose" tilewidth="64" tileheight="65" tilecount="6" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="64" height="64" source="../../assets/herose/Hero_Pistol/Hero_Pistol.png"/>
+ </tile>
+ <tile id="1">
+  <image width="41" height="39" source="../../../../../Downloads/zombi2 (1).png"/>
+ </tile>
+ <tile id="2">
+  <image width="41" height="48" source="../../../../../Downloads/zombi2 (2).png"/>
+ </tile>
+ <tile id="3">
+  <image width="35" height="65" source="../../../../../Downloads/zombi2 (3).png"/>
+ </tile>
+ <tile id="4">
+  <image width="35" height="62" source="../../../../../Downloads/zombi2 (4).png"/>
+ </tile>
+ <tile id="5">
+  <image width="47" height="58" source="../../../../../Downloads/zombi2 (5).png"/>
  </tile>
 </tileset>
--- resources/locations/summer_island/Trees.tsx
+++ resources/locations/summer_island/Trees.tsx
@@ -2,33 +2,63 @@
 <tileset version="1.9" tiledversion="1.9.2" name="Trees" tilewidth="161" tileheight="145" tilecount="10" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
+  <properties>
+   <property name="is_solid" type="bool" value="true"/>
+  </properties>
   <image width="29" height="28" source="../../assets/objects/Trees Bushes/TDS04_0006_Tree07.png"/>
  </tile>
  <tile id="1">
+  <properties>
+   <property name="is_solid" type="bool" value="true"/>
+  </properties>
   <image width="25" height="26" source="../../assets/objects/Trees Bushes/TDS04_0007_Tree06.png"/>
  </tile>
  <tile id="2">
+  <properties>
+   <property name="is_solid" type="bool" value="true"/>
+  </properties>
   <image width="45" height="45" source="../../assets/objects/Trees Bushes/TDS04_0008_Tree05.png"/>
  </tile>
  <tile id="3">
+  <properties>
+   <property name="is_solid" type="bool" value="true"/>
+  </properties>
   <image width="44" height="34" source="../../assets/objects/Trees Bushes/TDS04_0010_Bush-03.png"/>
  </tile>
  <tile id="4">
+  <properties>
+   <property name="is_solid" type="bool" value="true"/>
+  </properties>
   <image width="66" height="28" source="../../assets/objects/Trees Bushes/TDS04_0011_Bush-02.png"/>
  </tile>
  <tile id="5">
+  <properties>
+   <property name="is_solid" type="bool" value="true"/>
+  </properties>
   <image width="31" height="30" source="../../assets/objects/Trees Bushes/TDS04_0012_Bush-01.png"/>
  </tile>
  <tile id="6">
+  <properties>
+   <property name="is_solid" type="bool" value="true"/>
+  </properties>
   <image width="140" height="131" source="../../assets/objects/Trees Bushes/TDS04_0019_Tree4.png"/>
  </tile>
  <tile id="7">
+  <properties>
+   <property name="is_solid" type="bool" value="true"/>
+  </properties>
   <image width="161" height="145" source="../../assets/objects/Trees Bushes/TDS04_0020_Tree3.png"/>
  </tile>
  <tile id="8">
+  <properties>
+   <property name="is_solid" type="bool" value="true"/>
+  </properties>
   <image width="104" height="106" source="../../assets/objects/Trees Bushes/TDS04_0021_Tree2.png"/>
  </tile>
  <tile id="9">
+  <properties>
+   <property name="is_solid" type="bool" value="true"/>
+  </properties>
   <image width="152" height="136" source="../../assets/objects/Trees Bushes/TDS04_0022_Tree1.png"/>
  </tile>
 </tileset>
