e1ed62132ed2841852964494ce45bf74b760d662
Mark Meliksetyan <markmelix@gmail.com>
2022-12-19 16:57:12 +0300

    feat: remove unneeded chrono.Time component
    There's alredy pygame.time.get_ticks function for getting milliseconds
    from game start. Use it instead of using own Time component and
    processor for getting time from game start.

--- src/main.py
+++ src/main.py
@@ -12,14 +12,13 @@ from animation import (
     StateHandlingProcessor,
 )
 from input import InputProcessor
-from ui import UiDrawingProcessor
 from camera import CameraProcessor
 from roof import RoofTogglingProcessor
+from chrono import DayNightCyclingProcessor
 from creature import Creature, PlayerMarker
 from render import RenderProcessor, Renderable
 from utils import FPS, RESOLUTION, ResourcePath
 from location import Location, InitLocationProcessor, Position
-from chrono import Time, TimeMovingProcessor, DayNightCyclingProcessor
 from chunk import ChunkUnloadingProcessor, ChunkLoadingProcessor
 from movement import Direction, MovementProcessor, RotationProcessor, Velocity
 
@@ -36,8 +35,6 @@ PROCESSORS = (
     RoofTogglingProcessor,
     RenderProcessor,
     DayNightCyclingProcessor,
-    UiDrawingProcessor,
-    TimeMovingProcessor,
 )
 
 CHUNK_LOADER_PROCESSORS = (
@@ -47,7 +44,6 @@ CHUNK_LOADER_PROCESSORS = (
 
 
 def fill_world(world: esper.World):
-    world.create_entity(Time())
     location = world.create_entity(Location())
     player_surface = pygame.transform.rotate(
         pygame.transform.scale2x(
