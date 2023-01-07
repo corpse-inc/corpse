4ce72749dceda88a41084c97a3d57c8be612a2e6
Alchemmist <anton.ingrish@gmail.com>
2023-01-07 21:29:07 +0300

    feat: add boats on bitch

--- resources/locations/summer_island/Ground_as_tiles.tsx
+++ resources/locations/summer_island/Ground_as_tiles.tsx
@@ -3,22 +3,102 @@
  <image source="../../assets/background/Source/Tileset.png" width="1440" height="1024"/>
  <tile id="100">
   <properties>
-   <property name="is_wather" type="bool" value="true"/>
+   <property name="is_wother" type="bool" value="true"/>
   </properties>
  </tile>
  <tile id="101">
   <properties>
-   <property name="is_wather" type="bool" value="true"/>
+   <property name="is_wother" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="138">
+  <properties>
+   <property name="is_wother" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="139">
+  <properties>
+   <property name="is_wother" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="140">
+  <properties>
+   <property name="is_wother" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="141">
+  <properties>
+   <property name="is_wother" type="bool" value="true"/>
   </properties>
  </tile>
  <tile id="145">
   <properties>
-   <property name="is_wather" type="bool" value="true"/>
+   <property name="is_wother" type="bool" value="true"/>
   </properties>
  </tile>
  <tile id="146">
   <properties>
-   <property name="is_wather" type="bool" value="true"/>
+   <property name="is_wother" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="183">
+  <properties>
+   <property name="is_wother" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="184">
+  <properties>
+   <property name="is_wother" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="185">
+  <properties>
+   <property name="is_wother" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="186">
+  <properties>
+   <property name="is_wother" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="228">
+  <properties>
+   <property name="is_wother" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="229">
+  <properties>
+   <property name="is_wother" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="230">
+  <properties>
+   <property name="is_wother" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="231">
+  <properties>
+   <property name="is_wother" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="273">
+  <properties>
+   <property name="is_wother" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="274">
+  <properties>
+   <property name="is_wother" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="275">
+  <properties>
+   <property name="is_wother" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="276">
+  <properties>
+   <property name="is_wother" type="bool" value="true"/>
   </properties>
  </tile>
 </tileset>
