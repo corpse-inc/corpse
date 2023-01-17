ab248e7ef01acce115bbbd1b14a4976ed401bbe1
Alchemmist <anton.ingrish@gmail.com>
2023-01-18 01:40:02 +0300

    improvement

--- resources/locations/summer_island/Herose.tsx
+++ resources/locations/summer_island/Herose.tsx
@@ -1,35 +1,35 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Herose" tilewidth="218" tileheight="231" tilecount="20" columns="0">
+<tileset version="1.9" tiledversion="1.9.2" name="Herose" tilewidth="218" tileheight="231" tilecount="23" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="64" height="64" source="../../assets/herose/Hero_Pistol/Hero_Pistol.png"/>
  </tile>
  <tile id="1">
-  <image width="41" height="39" source="../../creatures/zombi/zombi2 (1).png"/>
+  <image width="41" height="39" source="../../assets/creatures/zombi/zombi2 (1).png"/>
  </tile>
  <tile id="2">
-  <image width="41" height="48" source="../../creatures/zombi/zombi2 (2).png"/>
+  <image width="41" height="48" source="../../assets/creatures/zombi/zombi2 (2).png"/>
  </tile>
  <tile id="3">
-  <image width="35" height="65" source="../../creatures/zombi/zombi2 (3).png"/>
+  <image width="35" height="65" source="../../assets/creatures/zombi/zombi2 (3).png"/>
  </tile>
  <tile id="4">
-  <image width="35" height="62" source="../../creatures/zombi/zombi2 (4).png"/>
+  <image width="35" height="62" source="../../assets/creatures/zombi/zombi2 (4).png"/>
  </tile>
  <tile id="5">
-  <image width="47" height="58" source="../../creatures/zombi/zombi2 (5).png"/>
+  <image width="47" height="58" source="../../assets/creatures/zombi/zombi2 (5).png"/>
  </tile>
  <tile id="7">
-  <image width="47" height="74" source="../../creatures/zombi/zombi2 (6).png"/>
+  <image width="47" height="74" source="../../assets/creatures/zombi/zombi2 (6).png"/>
  </tile>
  <tile id="8">
-  <image width="39" height="41" source="../../creatures/zombi/zombi2 (7).png"/>
+  <image width="39" height="41" source="../../assets/creatures/zombi/zombi2 (7).png"/>
  </tile>
  <tile id="9">
-  <image width="53" height="59" source="../../creatures/zombi/zombi2 (8).png"/>
+  <image width="53" height="59" source="../../assets/creatures/zombi/zombi2 (8).png"/>
  </tile>
  <tile id="10">
-  <image width="37" height="40" source="../../creatures/zombi/zombi2 (9).png"/>
+  <image width="37" height="40" source="../../assets/creatures/zombi/zombi2 (9).png"/>
  </tile>
  <tile id="11">
   <image width="218" height="138" source="../../assets/creatures/zombi/zombi (1).png"/>
@@ -60,5 +60,14 @@
  </tile>
  <tile id="20">
   <image width="110" height="118" source="../../assets/creatures/zombi/zombi (10).png"/>
+ </tile>
+ <tile id="21">
+  <image width="44" height="42" source="../../assets/creatures/zombi/zombi2 (10).png"/>
+ </tile>
+ <tile id="22">
+  <image width="41" height="44" source="../../assets/creatures/zombi/zombi2 (11).png"/>
+ </tile>
+ <tile id="23">
+  <image width="41" height="44" source="../../assets/creatures/zombi/zombi2 (12).png"/>
  </tile>
 </tileset>
