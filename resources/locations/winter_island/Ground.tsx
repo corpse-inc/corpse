2b1ea9b261cbcf6f05ca874dcb3a76639a423097
Alchemmist <anton.ingrish@gmail.com>
2023-01-17 07:39:19 +0300

    winter crash

---
+++ resources/locations/winter_island/Ground.tsx
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Ground" tilewidth="320" tileheight="320" tilecount="16" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <properties>
+   <property name="is_wather" type="bool" value="true"/>
+  </properties>
+  <image width="320" height="320" source="../../assets/background/Tiles/_0000_WTiles.png"/>
+ </tile>
+ <tile id="2">
+  <image width="320" height="320" source="../../assets/background/Tiles/_0001_DirtTiles.png"/>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="is_sand" type="bool" value="true"/>
+  </properties>
+  <image width="320" height="320" source="../../assets/background/Tiles/_0002_SandTiles.png"/>
+ </tile>
+ <tile id="4">
+  <image width="320" height="320" source="../../assets/background/Tiles/_0003_GrassTiles.png"/>
+ </tile>
+ <tile id="5">
+  <image width="320" height="320" source="../../assets/background/Tiles/_0004_RoadTiles.png"/>
+ </tile>
+ <tile id="6">
+  <image width="320" height="320" source="../../assets/background/Tiles/_0005_RoadDecals.png"/>
+ </tile>
+ <tile id="7">
+  <image width="320" height="320" source="../../assets/background/Tiles/_0006_GrassToRoad.png"/>
+ </tile>
+ <tile id="8">
+  <image width="320" height="320" source="../../assets/background/Tiles/_0007_SandToRoad.png"/>
+ </tile>
+ <tile id="9">
+  <image width="320" height="320" source="../../assets/background/Tiles/_0008_DirtToRoad.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="192" source="../../assets/background/Tiles/BridgeTiles.png"/>
+ </tile>
+ <tile id="11">
+  <image width="171" height="173" source="../../assets/background/Tiles/TDS04_0001_RoadSand.png"/>
+ </tile>
+ <tile id="12">
+  <image width="64" height="64" source="../../assets/background/Tiles/grass.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="64" source="../../assets/background/Tiles/land.png"/>
+ </tile>
+ <tile id="14">
+  <image width="64" height="64" source="../../assets/background/Tiles/sand.png"/>
+ </tile>
+ <tile id="15">
+  <image width="64" height="64" source="../../assets/background/Tiles/tarmac.png"/>
+ </tile>
+ <tile id="16">
+  <image width="64" height="64" source="../../assets/background/Tiles/whater.png"/>
+ </tile>
+</tileset>
