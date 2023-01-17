eec6a3dccee47e1afe39e8057e5f1cd0790e3b68
user36672 <anton.ingrish@gmail.com>
2023-01-17 15:57:36 +0300

    feat: add winter map

--- resources/locations/summer_island/Herose.tsx
+++ resources/locations/summer_island/Herose.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Herose" tilewidth="64" tileheight="74" tilecount="10" columns="0">
+<tileset version="1.9" tiledversion="1.9.2" name="Herose" tilewidth="218" tileheight="231" tilecount="20" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="64" height="64" source="../../assets/herose/Hero_Pistol/Hero_Pistol.png"/>
@@ -30,5 +30,35 @@
  </tile>
  <tile id="10">
   <image width="37" height="40" source="../../creatures/zombi/zombi2 (9).png"/>
+ </tile>
+ <tile id="11">
+  <image width="218" height="138" source="../../assets/creatures/zombi/zombi (1).png"/>
+ </tile>
+ <tile id="12">
+  <image width="118" height="122" source="../../assets/creatures/zombi/zombi (2).png"/>
+ </tile>
+ <tile id="13">
+  <image width="106" height="118" source="../../assets/creatures/zombi/zombi (3).png"/>
+ </tile>
+ <tile id="14">
+  <image width="113" height="98" source="../../assets/creatures/zombi/zombi (4).png"/>
+ </tile>
+ <tile id="15">
+  <image width="118" height="231" source="../../assets/creatures/zombi/zombi (5).png"/>
+ </tile>
+ <tile id="16">
+  <image width="130" height="130" source="../../assets/creatures/zombi/zombi (6).png"/>
+ </tile>
+ <tile id="17">
+  <image width="110" height="118" source="../../assets/creatures/zombi/zombi (7).png"/>
+ </tile>
+ <tile id="18">
+  <image width="106" height="118" source="../../assets/creatures/zombi/zombi (8).png"/>
+ </tile>
+ <tile id="19">
+  <image width="214" height="210" source="../../assets/creatures/zombi/zombi (9).png"/>
+ </tile>
+ <tile id="20">
+  <image width="110" height="118" source="../../assets/creatures/zombi/zombi (10).png"/>
  </tile>
 </tileset>
