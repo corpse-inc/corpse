174029ad964cdd7316245587dfb3f8ab053f1868
Alchemmist <anton.ingrish@gmail.com>
2023-01-18 04:13:19 +0300

    fix

---
+++ resources/locations/winter_island/autumn_ground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Ground_as_tiles" tilewidth="32" tileheight="32" tilecount="1440" columns="45">
+ <image source="../../assets_autumn/background/Source/Ground_as_tiles.png" width="1440" height="1024"/>
+</tileset>
