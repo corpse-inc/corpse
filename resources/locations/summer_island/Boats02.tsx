5a553080d0d8cb7733496d77eb6eae43635be566
Alchemmist <anton.ingrish@gmail.com>
2023-01-19 04:28:41 +0300

    finsh summer island. Rename tilesets

--- resources/locations/summer_island/Boats02.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Source" tilewidth="32" tileheight="32" tilecount="1320" columns="30">
- <image source="../../assets/boats/Source.png" width="963" height="1421"/>
-</tileset>
