80a4795057e44a397f65e562e96a7c27f3dd4eca
Alchemmist <anton.ingrish@gmail.com>
2023-01-19 07:14:59 +0300

    refactor 0.1

---
+++ data/world/summer_island/objects.tsx
@@ -0,0 +1,88 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Objects" tilewidth="85" tileheight="64" tilecount="14" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <properties>
+   <property name="is_solid" type="bool" value="true"/>
+  </properties>
+  <image width="30" height="31" source="../../assets/summer/objects/box/TDS04_0013_Box-02.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="is_solid" type="bool" value="true"/>
+  </properties>
+  <image width="19" height="19" source="../../assets/summer/objects/box/TDS04_0014_Box-02-mini.png"/>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="is_solid" type="bool" value="true"/>
+  </properties>
+  <image width="20" height="20" source="../../assets/summer/objects/box/TDS04_0015_Barrel-oil.png"/>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="is_solid" type="bool" value="true"/>
+  </properties>
+  <image width="20" height="20" source="../../assets/summer/objects/box/TDS04_0016_Barrel.png"/>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="is_solid" type="bool" value="true"/>
+  </properties>
+  <image width="19" height="19" source="../../assets/summer/objects/box/TDS04_0017_Box1-mini.png"/>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="is_solid" type="bool" value="true"/>
+  </properties>
+  <image width="30" height="31" source="../../assets/summer/objects/box/TDS04_0018_Box1.png"/>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="is_solid" type="bool" value="true"/>
+  </properties>
+  <image width="56" height="64" source="../../assets/summer/objects/Rocks/TDS04_0003_Rock03.png"/>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="is_solid" type="bool" value="true"/>
+  </properties>
+  <image width="48" height="62" source="../../assets/summer/objects/Rocks/TDS04_0004_Rock02.png"/>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="is_solid" type="bool" value="true"/>
+  </properties>
+  <image width="31" height="38" source="../../assets/summer/objects/Rocks/TDS04_0005_Rock01.png"/>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="is_solid" type="bool" value="true"/>
+  </properties>
+  <image width="85" height="23" source="../../assets/summer/objects/SandBag/TDS04_0002_Sandbags.png"/>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="is_solid" type="bool" value="true"/>
+  </properties>
+  <image width="30" height="30" source="../../assets/summer/objects/beach/Sprite-0002.png"/>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="is_solid" type="bool" value="true"/>
+  </properties>
+  <image width="60" height="30" source="../../assets/summer/objects/beach/shizlong.png"/>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="is_solid" type="bool" value="true"/>
+  </properties>
+  <image width="30" height="30" source="../../assets/summer/objects/beach/zont_broken.png"/>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="is_solid" type="bool" value="true"/>
+  </properties>
+  <image width="60" height="30" source="../../assets/summer/objects/beach/shizlong2.png"/>
+ </tile>
+</tileset>
--- data/world/tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Tileset" tilewidth="64" tileheight="64" tilecount="352" columns="22">
- <image source="../assets/tds_modern_pixel_game_kit/tilesets/PNG/Source/Tileset.png" width="1440" height="1024"/>
-</tileset>
