16a78b844ed709588e0e8d4051dbac9a1d062d85
Alchemmist <anton.ingrish@gmail.com>
2023-01-21 20:48:40 +0300

    feat: set aligment to center

--- data/world/winter_island/tilesets/tileset.tsx
+++ data/world/winter_island/tilesets/tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Tileset" tilewidth="32" tileheight="32" tilecount="1440" columns="45">
+<tileset version="1.9" tiledversion="1.9.2" name="Tileset" tilewidth="32" tileheight="32" tilecount="1440" columns="45" objectalignment="center">
  <image source="../../../assets/winter/background/Source/floor.png" width="1440" height="1024"/>
 </tileset>
