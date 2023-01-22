f00a0bce63e863c127c263541f196c26f35e9a41
Alchemmist <anton.ingrish@gmail.com>
2023-01-22 23:40:07 +0300

    basic fix on winter map

--- data/world/winter_island/tilesets/autumn_trees.tsx
+++ data/world/winter_island/tilesets/autumn_trees.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="autumn_trees" tilewidth="161" tileheight="145" tilecount="17" columns="0">
+<tileset version="1.9" tiledversion="1.9.2" name="autumn_trees" tilewidth="161" tileheight="145" tilecount="17" columns="0" objectalignment="center">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="152" height="136" source="../../../assets/autumn/objects/trees/1.png"/>
