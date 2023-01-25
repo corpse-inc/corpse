21645f8cbd782bccc8fd78582785168a35b35874
user36672 <anton.ingrish@gamil.com>
2023-01-25 09:06:56 +0300

    fix transparent water on winter island

--- data/world/winter_island/fix_visible_scripte.py
+++ data/world/winter_island/fix_visible_scripte.py
@@ -1,11 +1,11 @@
-with open("map - Copy.txt", mode='r', encoding='utf-8') as map: 
+with open("map.txt", mode='r', encoding='utf-8') as map:
     lines = map.readlines() 
     refactor_lines = []
     for i in lines:
         refactor_i = i
         if 'visible' in i:
-            refactor_i = i[0:i.rfind('visible') - 1] + '/>'     
+            refactor_i = ''
         refactor_lines.append(refactor_i)
 
-with open('map - Copy.tmx', mode='w', encoding='utf-8') as new_map:
+with open('map.tmx', mode='w', encoding='utf-8') as new_map:
     new_map.write(''.join(refactor_lines))
