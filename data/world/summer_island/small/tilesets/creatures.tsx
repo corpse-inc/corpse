483df078cb838ffebe2b8c8d9a05e4bc88cb939e
Alchemmist <anton.ingrish@gmail.com>
2023-01-25 23:35:40 +0300

    fix

--- data/world/summer_island/small/tilesets/creatures.tsx
+++ data/world/summer_island/small/tilesets/creatures.tsx
@@ -5,160 +5,160 @@
   <properties>
    <property name="creature" value="zombie"/>
   </properties>
-  <image width="64" height="64" source="../../../assets/summer/creatures/Hero_Pistol/Hero_Pistol.png"/>
+  <image width="64" height="64" source="../../../../assets/summer/creatures/Hero_Pistol/Hero_Pistol.png"/>
  </tile>
  <tile id="1">
   <properties>
    <property name="creature" value="zombie"/>
    <property name="soft" value="false"/>
   </properties>
-  <image width="41" height="39" source="../../../assets/summer/creatures/zombie/zombi2 (1).png"/>
+  <image width="41" height="39" source="../../../../assets/summer/creatures/zombie/zombi2 (1).png"/>
  </tile>
  <tile id="2">
   <properties>
    <property name="creature" value="zombie"/>
    <property name="soft" value="false"/>
   </properties>
-  <image width="41" height="48" source="../../../assets/summer/creatures/zombie/zombi2 (2).png"/>
+  <image width="41" height="48" source="../../../../assets/summer/creatures/zombie/zombi2 (2).png"/>
  </tile>
  <tile id="3">
   <properties>
    <property name="creature" value="zombie"/>
    <property name="soft" value="false"/>
   </properties>
-  <image width="35" height="65" source="../../../assets/summer/creatures/zombie/zombi2 (3).png"/>
+  <image width="35" height="65" source="../../../../assets/summer/creatures/zombie/zombi2 (3).png"/>
  </tile>
  <tile id="4">
   <properties>
    <property name="creature" value="zombie"/>
    <property name="soft" value="false"/>
   </properties>
-  <image width="35" height="62" source="../../../assets/summer/creatures/zombie/zombi2 (4).png"/>
+  <image width="35" height="62" source="../../../../assets/summer/creatures/zombie/zombi2 (4).png"/>
  </tile>
  <tile id="5">
   <properties>
    <property name="creature" value="zombie"/>
    <property name="soft" value="false"/>
   </properties>
-  <image width="47" height="58" source="../../../assets/summer/creatures/zombie/zombi2 (5).png"/>
+  <image width="47" height="58" source="../../../../assets/summer/creatures/zombie/zombi2 (5).png"/>
  </tile>
  <tile id="7">
   <properties>
    <property name="creature" value="zombie"/>
    <property name="soft" value="false"/>
   </properties>
-  <image width="47" height="74" source="../../../assets/summer/creatures/zombie/zombi2 (6).png"/>
+  <image width="47" height="74" source="../../../../assets/summer/creatures/zombie/zombi2 (6).png"/>
  </tile>
  <tile id="8">
   <properties>
    <property name="creature" value="zombie"/>
    <property name="soft" value="false"/>
   </properties>
-  <image width="39" height="41" source="../../../assets/summer/creatures/zombie/zombi2 (7).png"/>
+  <image width="39" height="41" source="../../../../assets/summer/creatures/zombie/zombi2 (7).png"/>
  </tile>
  <tile id="9">
   <properties>
    <property name="creature" value="zombie"/>
    <property name="soft" value="false"/>
   </properties>
-  <image width="53" height="59" source="../../../assets/summer/creatures/zombie/zombi2 (8).png"/>
+  <image width="53" height="59" source="../../../../assets/summer/creatures/zombie/zombi2 (8).png"/>
  </tile>
  <tile id="10">
   <properties>
    <property name="creature" value="zombie"/>
    <property name="soft" value="false"/>
   </properties>
-  <image width="37" height="40" source="../../../assets/summer/creatures/zombie/zombi2 (9).png"/>
+  <image width="37" height="40" source="../../../../assets/summer/creatures/zombie/zombi2 (9).png"/>
  </tile>
  <tile id="11">
   <properties>
    <property name="creature" value="zombie"/>
    <property name="soft" value="false"/>
   </properties>
-  <image width="218" height="138" source="../../../assets/summer/creatures/zombie/zombi (1).png"/>
+  <image width="218" height="138" source="../../../../assets/summer/creatures/zombie/zombi (1).png"/>
  </tile>
  <tile id="12">
   <properties>
    <property name="creature" value="zombie"/>
    <property name="soft" value="false"/>
   </properties>
-  <image width="118" height="122" source="../../../assets/summer/creatures/zombie/zombi (2).png"/>
+  <image width="118" height="122" source="../../../../assets/summer/creatures/zombie/zombi (2).png"/>
  </tile>
  <tile id="13">
   <properties>
    <property name="creature" value="zombie"/>
    <property name="soft" value="false"/>
   </properties>
-  <image width="106" height="118" source="../../../assets/summer/creatures/zombie/zombi (3).png"/>
+  <image width="106" height="118" source="../../../../assets/summer/creatures/zombie/zombi (3).png"/>
  </tile>
  <tile id="14">
   <properties>
    <property name="creature" value="zombie"/>
    <property name="soft" value="false"/>
   </properties>
-  <image width="113" height="98" source="../../../assets/summer/creatures/zombie/zombi (4).png"/>
+  <image width="113" height="98" source="../../../../assets/summer/creatures/zombie/zombi (4).png"/>
  </tile>
  <tile id="15">
   <properties>
    <property name="creature" value="zombie"/>
    <property name="soft" value="false"/>
   </properties>
-  <image width="118" height="231" source="../../../assets/summer/creatures/zombie/zombi (5).png"/>
+  <image width="118" height="231" source="../../../../assets/summer/creatures/zombie/zombi (5).png"/>
  </tile>
  <tile id="16">
   <properties>
    <property name="creature" value="zombie"/>
    <property name="soft" value="false"/>
   </properties>
-  <image width="130" height="130" source="../../../assets/summer/creatures/zombie/zombi (6).png"/>
+  <image width="130" height="130" source="../../../../assets/summer/creatures/zombie/zombi (6).png"/>
  </tile>
  <tile id="17">
   <properties>
    <property name="creature" value="zombie"/>
    <property name="soft" value="false"/>
   </properties>
-  <image width="110" height="118" source="../../../assets/summer/creatures/zombie/zombi (7).png"/>
+  <image width="110" height="118" source="../../../../assets/summer/creatures/zombie/zombi (7).png"/>
  </tile>
  <tile id="18">
   <properties>
    <property name="creature" value="zombie"/>
    <property name="soft" value="false"/>
   </properties>
-  <image width="106" height="118" source="../../../assets/summer/creatures/zombie/zombi (8).png"/>
+  <image width="106" height="118" source="../../../../assets/summer/creatures/zombie/zombi (8).png"/>
  </tile>
  <tile id="19">
   <properties>
    <property name="creature" value="zombie"/>
    <property name="soft" value="false"/>
   </properties>
-  <image width="214" height="210" source="../../../assets/summer/creatures/zombie/zombi (9).png"/>
+  <image width="214" height="210" source="../../../../assets/summer/creatures/zombie/zombi (9).png"/>
  </tile>
  <tile id="20">
   <properties>
    <property name="creature" value="zombie"/>
    <property name="soft" value="false"/>
   </properties>
-  <image width="110" height="118" source="../../../assets/summer/creatures/zombie/zombi (10).png"/>
+  <image width="110" height="118" source="../../../../assets/summer/creatures/zombie/zombi (10).png"/>
  </tile>
  <tile id="21">
   <properties>
    <property name="creature" value="zombie"/>
    <property name="soft" value="false"/>
   </properties>
-  <image width="44" height="42" source="../../../assets/summer/creatures/zombie/zombi2 (10).png"/>
+  <image width="44" height="42" source="../../../../assets/summer/creatures/zombie/zombi2 (10).png"/>
  </tile>
  <tile id="22">
   <properties>
    <property name="creature" value="zombie"/>
    <property name="soft" value="false"/>
   </properties>
-  <image width="41" height="44" source="../../../assets/summer/creatures/zombie/zombi2 (11).png"/>
+  <image width="41" height="44" source="../../../../assets/summer/creatures/zombie/zombi2 (11).png"/>
  </tile>
  <tile id="23">
   <properties>
    <property name="creature" value="zombie"/>
    <property name="soft" value="false"/>
   </properties>
-  <image width="41" height="44" source="../../../assets/summer/creatures/zombie/zombi2 (12).png"/>
+  <image width="41" height="44" source="../../../../assets/summer/creatures/zombie/zombi2 (12).png"/>
  </tile>
 </tileset>
--- data/world/summer_island/small/tilesets/ground.tsx
+++ data/world/summer_island/small/tilesets/ground.tsx
@@ -5,51 +5,51 @@
   <properties>
    <property name="is_wather" type="bool" value="true"/>
   </properties>
-  <image width="320" height="320" source="../../../assets/summer/background/tiles/_0000_WTiles.png"/>
+  <image width="320" height="320" source="../../../../assets/summer/background/tiles/_0000_WTiles.png"/>
  </tile>
  <tile id="2">
-  <image width="320" height="320" source="../../../assets/summer/background/tiles/_0001_DirtTiles.png"/>
+  <image width="320" height="320" source="../../../../assets/summer/background/tiles/_0001_DirtTiles.png"/>
  </tile>
  <tile id="3">
-  <image width="320" height="320" source="../../../assets/summer/background/tiles/_0002_SandTiles.png"/>
+  <image width="320" height="320" source="../../../../assets/summer/background/tiles/_0002_SandTiles.png"/>
  </tile>
  <tile id="4">
-  <image width="320" height="320" source="../../../assets/summer/background/tiles/_0003_GrassTiles.png"/>
+  <image width="320" height="320" source="../../../../assets/summer/background/tiles/_0003_GrassTiles.png"/>
  </tile>
  <tile id="5">
-  <image width="320" height="320" source="../../../assets/summer/background/tiles/_0004_RoadTiles.png"/>
+  <image width="320" height="320" source="../../../../assets/summer/background/tiles/_0004_RoadTiles.png"/>
  </tile>
  <tile id="6">
-  <image width="320" height="320" source="../../../assets/summer/background/tiles/_0005_RoadDecals.png"/>
+  <image width="320" height="320" source="../../../../assets/summer/background/tiles/_0005_RoadDecals.png"/>
  </tile>
  <tile id="7">
-  <image width="320" height="320" source="../../../assets/summer/background/tiles/_0006_GrassToRoad.png"/>
+  <image width="320" height="320" source="../../../../assets/summer/background/tiles/_0006_GrassToRoad.png"/>
  </tile>
  <tile id="8">
-  <image width="320" height="320" source="../../../assets/summer/background/tiles/_0007_SandToRoad.png"/>
+  <image width="320" height="320" source="../../../../assets/summer/background/tiles/_0007_SandToRoad.png"/>
  </tile>
  <tile id="9">
-  <image width="320" height="320" source="../../../assets/summer/background/tiles/_0008_DirtToRoad.png"/>
+  <image width="320" height="320" source="../../../../assets/summer/background/tiles/_0008_DirtToRoad.png"/>
  </tile>
  <tile id="10">
-  <image width="64" height="192" source="../../../assets/summer/background/tiles/BridgeTiles.png"/>
+  <image width="64" height="192" source="../../../../assets/summer/background/tiles/BridgeTiles.png"/>
  </tile>
  <tile id="11">
-  <image width="171" height="173" source="../../../assets/summer/background/tiles/TDS04_0001_RoadSand.png"/>
+  <image width="171" height="173" source="../../../../assets/summer/background/tiles/TDS04_0001_RoadSand.png"/>
  </tile>
  <tile id="12">
-  <image width="64" height="64" source="../../../assets/summer/background/tiles/grass.png"/>
+  <image width="64" height="64" source="../../../../assets/summer/background/tiles/grass.png"/>
  </tile>
  <tile id="13">
-  <image width="64" height="64" source="../../../assets/summer/background/tiles/land.png"/>
+  <image width="64" height="64" source="../../../../assets/summer/background/tiles/land.png"/>
  </tile>
  <tile id="14">
-  <image width="64" height="64" source="../../../assets/summer/background/tiles/sand.png"/>
+  <image width="64" height="64" source="../../../../assets/summer/background/tiles/sand.png"/>
  </tile>
  <tile id="15">
-  <image width="64" height="64" source="../../../assets/summer/background/tiles/tarmac.png"/>
+  <image width="64" height="64" source="../../../../assets/summer/background/tiles/tarmac.png"/>
  </tile>
  <tile id="16">
-  <image width="64" height="64" source="../../../assets/summer/background/tiles/whater.png"/>
+  <image width="64" height="64" source="../../../../assets/summer/background/tiles/whater.png"/>
  </tile>
 </tileset>
